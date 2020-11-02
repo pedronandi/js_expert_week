d4b682b1ba37c3d2cfdcccb4116df0460e228835
Pedro Nandi <pedronandi@gmail.com>
2020-11-02 14:51:44 +0000

    Arrumando o host

--- public/bandersnatch/index.js
+++ public/bandersnatch/index.js
@@ -1,5 +1,5 @@
 const MANIFEST_URL = 'manifest.json'
-const localHost = ['127.0.0.1', 'localhost']
+const localHost = ['127.0.0.1', '172.17.0.2', 'localhost']
 
 async function main() {
     const isLocal = !!~localHost.indexOf(window.location.hostname)  
